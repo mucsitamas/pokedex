2bd31e61679beda0cfcca65fc8e0830d8177e056
mucsi.tamas <mucsi.tamas@wwdh.hu>
2017-10-29 19:38:14 +0100

    commit

---
+++ pokedex/Model/Pokemon.swift
@@ -0,0 +1,29 @@
+//
+//  Pokemon.swift
+//  pokedex
+//
+//  Created by Mucsi Tamás on 2017. 10. 29..
+//  Copyright © 2017. Mucsi Tamás. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+}
