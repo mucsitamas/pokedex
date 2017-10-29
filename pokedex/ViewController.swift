e786649afb0d42cee762c050188ee2146ea6ad58
mucsi.tamas <mucsi.tamas@wwdh.hu>
2017-10-29 17:19:41 +0100

    init

---
+++ pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pokedex
+//
+//  Created by Mucsi Tamás on 2017. 10. 29..
+//  Copyright © 2017. Mucsi Tamás. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
